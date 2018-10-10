bd74f9f444275ae9391e6d400d043a4c12884c7b
itsdickson <itsklein@hotmail.com>
2018-10-11 02:31:46 +0800

    temporary update to style

--- src/viewproject.php
+++ src/viewproject.php
@@ -6,8 +6,114 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CrowdFund - View Project</title>
     <!-- <link rel="stylesheet" href="style.css"> -->
-    <!-- <link rel="stylesheet" <link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet"> -->
-	<!-- <style>li {list-style: none;}</style> -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link rel="stylesheet" <link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
+
+    <style media="screen">
+
+     nav{
+      background-color: black;
+    }
+
+    nav ul{
+      margin:0;
+      list-style-type: none;
+      text-align: center;
+      text-color: white;
+    }
+
+    nav ul li{
+      display: inline-block;
+      text-align: center;
+      padding: 20px;
+      height: 2%;
+      text-color: white;
+    }
+
+    nav ul li a{
+      text-decoration: none;
+      color: white;
+      padding: 20px;
+    }
+    nav ul li a:hover, nav ul li a:active{
+      border-bottom: 2px solid #ccc;
+      padding-bottom: 8px;
+      border: 1px solid #447314;
+      text-shadow: #31540c 0 1px 0;
+      background: #6aa436;
+      background-image: -ms-linear-gradient(top, #8dc059 0%, #6aa436 100%);
+      color: #fff;
+    }
+    .form-container {
+    background: white;
+    text-decoration: none;
+    text-align: center;
+
+
+     }
+  .form-field {
+     border: 2px solid #c9b7a2;
+     background: white;
+     color: black  ;
+     padding:8px;
+     width:280px;
+     }
+  .form-fieldLong {
+    border: 2px solid #c9b7a2;
+    background: white;
+    color: black;
+    padding:8px;
+    width:280px;
+    height: 70px;
+    }
+
+  .form-field:focus {
+     background: #fff;
+     border-color: #6CBEEC;
+     color: black;
+     }
+  .form-fieldLong:focus {
+    background: #fff;
+    border-color: #6CBEEC;
+    color: black;
+    }
+  .form-container h2 {
+     font-size:18px;
+     font-weight:bold;
+     text-align:center;
+      }
+  .form-title {
+     margin-bottom:10px;
+     color: black ;
+     }
+  .submit-container {
+     }
+  .submit-button {
+     border: 1px solid white;
+     background: black;
+     color: white;
+     padding: 8.5px 18px;
+     font-size: 14px;
+     text-decoration: none;
+     vertical-align: middle;
+     width: 300px;
+     }
+  .submit-button:hover {
+     border: 1px solid #447314;
+     text-shadow: #31540c 0 1px 0;
+     background: #6aa436;
+     background-image: -ms-linear-gradient(top, #8dc059 0%, #6aa436 100%);
+     color: #fff;
+     }
+  .submit-button:active {
+     text-shadow: #31540c 0 1px 0;
+     border: 1px solid #447314;
+     background: #8dc059;
+     background-image: -ms-linear-gradient(top, #6aa436 0%, #8dc059 100%);
+     color: #fff;
+     }
+
+  </style>
 
 </head>
 
@@ -15,7 +121,7 @@
 	session_start();
 	$db = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=4or2jsqi");
 	if (!db) {
-		echo "error connecting to DB.\n";
+		echo "An error occured when connecting to DB.\n";
 		exit;
 	}
 	$projectid = $_GET["projectid"];
@@ -29,30 +135,34 @@
 
 	if (isset($_POST['submit'])) {
 
-        $query2 = "INSERT INTO fund VALUES('$uemail', '$_POST[amountfunded]', '$projectid')";
-        $result2 = pg_query($db, $query2);
+    $query2 = "INSERT INTO fund VALUES('$uemail', '$_POST[amountfunded]', '$projectid')";
+    $result2 = pg_query($db, $query2);
 		if (!$result2) {
-			echo "failed";
+      echo '<script language="javascript">';
+      echo 'alert("Failed to add funds!")';
+      echo '</script>';
 		} else {
-			echo "done";
 			$query3 = "UPDATE project_advertised SET amountfund = amountfund + $_POST[amountfunded] WHERE projectid = '$projectid'";
 			$result3 = pg_query($db, $query3);
-			$query4 = "SELECT title, startdate, enddate, category, amountfund, targetamount, description, status FROM project_advertised WHERE projectid = '$projectid'";
-			$result4 = pg_query($db, $query4);
-			$row = pg_fetch_assoc($result4);
-			if (!result3) {
-				echo "update failed";
-			} else {
-				echo "update success";
-			}
-
-			if (!result4) {
-				echo "updated show failed";
-			} else {
-				echo "updated show success";
-			}
-		}
+      if (!$result3) {
+        echo '<script language="javascript">';
+        echo 'alert("Failed to update funds!")';
+        echo '</script>';
+      }
+	    $query4 = "SELECT title, startdate, enddate, category, amountfund, targetamount, description, status FROM project_advertised WHERE projectid = '$projectid'";
+      $result4 = pg_query($db, $query4);
+      $row = pg_fetch_assoc($result4);
+      if (!$result4) {
+        echo '<script language="javascript">';
+        echo 'alert("Failed to update funds!")';
+        echo '</script>';
+	    } else {
+        echo '<script language="javascript">';
+        echo 'alert("Successfully added fund!")';
+        echo '</script>';
+	    }
     }
+  }
 ?>
 
 <body>
@@ -75,7 +185,7 @@
 		</thead>
 		<tbody>
 			<?php
-				echo "<tr><td> Start Date: ".$row['startdate']."</td></tr>";
+				echo "<tr><td><div class="text-" Start Date: ".$row['startdate']."</td></tr>";
 				echo "<tr><td> End Date: ".$row['enddate']."</td></tr>";
 				echo "<tr><td> Category: ".$row['category']."</td></tr>";
 				echo "<tr><td> Description: ".$row['description']."</td></tr>";
@@ -89,7 +199,7 @@
 	<form name="view_project" action="viewproject.php?projectid="<?php echo $projectid;?> method="POST">
   <li>Fund Amount</li>
 	<input type="text" name="amountfunded" />
-	<input type="submit" name="submit" />
+	<input class="submit-button" type="submit" name="submit" />
 	</form>
 
 </body>
